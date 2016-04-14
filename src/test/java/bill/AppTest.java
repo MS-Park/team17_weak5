a51e5062aac82993e3ee79a49aff0fcb87cccc0a
FakerLulu <kjmjskr@gmail.com>
2016-04-14 18:33:37 +0900

    repair silverCal

--- src/test/java/bill/AppTest.java
+++ src/test/java/bill/AppTest.java
@@ -1,35 +1,17 @@
 package bill;
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class AppTest {
-/*	private Makebill bill;
-	
+	private Makebill bill;
+
 	@Test
-	public void nullbill() {
-		bill = new Makebill("sdf",0,0);
-		assertTrue(bill.totalBill()==0);
+	public void SilverBilloveruse(){
+		bill = new Makebill("Silver",1,1000);
+		assertTrue(bill.totalBill()==299.95);
 	}
-	@Test
-	public void goldBillNoneaddition() {
-		bill = new Makebill("Gold",1,2);
-		assertEquals(bill.totalBill(),49.95,0.005);
+	public void SilverBillInuse(){
+		bill = new Makebill("Silver",1,500);
+		assertEquals(bill.totalBill(),29.95,0.000);
 	}
-	@Test
-	public void goldBilladdLine() {
-		bill = new Makebill("Gold",3,878);
-		assertTrue(bill.totalBill()==78.95);
-	}
-	@Test
-	public void goldBillFamily() {
-		bill = new Makebill("Gold",4,878);
-		assertTrue(bill.totalBill()==83.95);
-	}
-	@Test
-	public void goldBilloveruse() {
-		bill = new Makebill("Gold",1,1010);
-		assertTrue(bill.totalBill()==54.45);
-	}
-*/	
 }
