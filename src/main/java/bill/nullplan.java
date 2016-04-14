8b5fbade654b0801f57b33fb43e43d3ea1330a12
Whoseop-Song <whoseop.song@gmail.com>
2016-04-14 17:48:22 +0900

    copy from team17-weak4 repo

---
+++ src/main/java/bill/nullplan.java
@@ -0,0 +1,25 @@
+package bill;
+
+public class nullplan implements Plan{
+
+	public double getBasicCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getOverchargeCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getAdditionalLineCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getFamilyDiscountcost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/test/java/bill/AppTest.java
@@ -0,0 +1,35 @@
+package bill;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+/*	private Makebill bill;
+	
+	@Test
+	public void nullbill() {
+		bill = new Makebill("sdf",0,0);
+		assertTrue(bill.totalBill()==0);
+	}
+	@Test
+	public void goldBillNoneaddition() {
+		bill = new Makebill("Gold",1,2);
+		assertEquals(bill.totalBill(),49.95,0.005);
+	}
+	@Test
+	public void goldBilladdLine() {
+		bill = new Makebill("Gold",3,878);
+		assertTrue(bill.totalBill()==78.95);
+	}
+	@Test
+	public void goldBillFamily() {
+		bill = new Makebill("Gold",4,878);
+		assertTrue(bill.totalBill()==83.95);
+	}
+	@Test
+	public void goldBilloveruse() {
+		bill = new Makebill("Gold",1,1010);
+		assertTrue(bill.totalBill()==54.45);
+	}
+*/	
+}
