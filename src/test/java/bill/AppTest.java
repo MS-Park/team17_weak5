a849d0c185876648e1794d428fd38c95dafd6664
Whoseop-Song <whoseop.song@gmail.com>
2016-04-14 19:50:05 +0900

    test비교대상 값 계산실수 수정

--- src/test/java/bill/AppTest.java
+++ src/test/java/bill/AppTest.java
@@ -13,61 +13,61 @@ public class AppTest {
 	@Test
 	public void goldBillNoaddNooveruse1() {
 		bill = new Makebill("Gold",1,2);
-		assertEquals(bill.totalBill(),49.95,0.005);
+		assertEquals(bill.totalBill(),49.95,0.01);
 	}
 	@Test
 	public void goldBillNoaddNooveruse2() {
 		bill = new Makebill("gold",1,2);
-		assertEquals(bill.totalBill(),49.95,0.005);
+		assertEquals(bill.totalBill(),49.95,0.01);
 	}
 	@Test
 	public void goldBillNoaddoveruse1() {
 		bill = new Makebill("Gold",1,3800);
-		assertEquals(bill.totalBill(),1324.45,0.005);
+		assertEquals(bill.totalBill(),1309.95,0.01);
 	}
 	@Test
 	public void goldBillNoaddoveruse2() {
 		bill = new Makebill("gold",1,3800);
-		assertEquals(bill.totalBill(),1324.45,0.005);
+		assertEquals(bill.totalBill(),1309.95,0.01);
 	}
 	@Test
 	public void goldBilladdNooveruse1() {
 		bill = new Makebill("Gold",3,2);
-		assertEquals(bill.totalBill(),78.95,0.005);
+		assertEquals(bill.totalBill(),78.95,0.01);
 	}
 	@Test
 	public void goldBilladdNooveruse2() {
 		bill = new Makebill("gold",3,2);
-		assertEquals(bill.totalBill(),78.95,0.005);
+		assertEquals(bill.totalBill(),78.95,0.01);
 	}
 	@Test
 	public void goldBilladdoveruse1() {
 		bill = new Makebill("Gold",3,3800);
-		assertEquals(bill.totalBill(),1338.95,0.005);
+		assertEquals(bill.totalBill(),1338.95,0.01);
 	}
 	@Test
 	public void goldBilladdoveruse2() {
 		bill = new Makebill("gold",3,3800);
-		assertEquals(bill.totalBill(),1338.95,0.005);
+		assertEquals(bill.totalBill(),1338.95,0.01);
 	}
 	@Test
 	public void goldBillFaddNooveruse1() {
 		bill = new Makebill("Gold",4,456);
-		assertEquals(bill.totalBill(),83.95,0.005);
+		assertEquals(bill.totalBill(),83.95,0.01);
 	}
 	@Test
 	public void goldBillFaddNooveruse2() {
 		bill = new Makebill("gold",4,456);
-		assertEquals(bill.totalBill(),83.95,0.005);
+		assertEquals(bill.totalBill(),83.95,0.01);
 	}
 	@Test
 	public void goldBillFaddoveruse1() {
 		bill = new Makebill("Gold",4,2000);
-		assertEquals(bill.totalBill(),533.95,0.005);
+		assertEquals(bill.totalBill(),533.95,0.01);
 	}
 	@Test
 	public void goldBillFaddoveruse2() {
 		bill = new Makebill("gold",4,2000);
-		assertEquals(bill.totalBill(),533.95,0.005);
+		assertEquals(bill.totalBill(),533.95,0.01);
 	}
 }
