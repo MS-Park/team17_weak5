2e86b5a708fc1ae9a7da5db463ce4d424b1a185a
Whoseop-Song <whoseop.song@gmail.com>
2016-04-14 19:55:36 +0900

    test오류와 Makebill오류 수정

--- src/main/java/bill/Makebill.java
+++ src/main/java/bill/Makebill.java
@@ -50,9 +50,9 @@ public class Makebill {
 	 * 초과사용 요금 리턴
 	 */
 	public double getOverchargeForUsage(){
-		if(usage>1000&&"Gold".equals(plan) ||"gold".equals(plan)){
+		if(usage>1000&&("Gold".equals(plan) ||"gold".equals(plan))){
 		         return (usage-1000)*planinfo.getOverchargeCost();
-		}else if(usage>500&&"Silver".equals(plan) ||"silver".equals(plan)){
+		}else if(usage>500&&("Silver".equals(plan) ||"silver".equals(plan))){
 				return (usage-500)*planinfo.getOverchargeCost();
 		}
 		else{
